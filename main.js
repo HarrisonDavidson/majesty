18e6e5c65cd1bf8fd2ca930e95718ef8c41de797
Harrison Davidson <hossdavidson@gmail.com>
2023-02-17 15:29:26 -0500

    majesty

--- main.js
+++ main.js
@@ -1,49 +1,49 @@
-const queens = []
-const tributeChest = []
+// const queens = []
+// const tributeChest = []
 
-const createQueen = (queenId, queenName) => {
-    const queenObject = {
-        id: queenId,
-        name: queenName
-    }
+// const createQueen = (queenId, queenName) => {
+//     const queenObject = {
+//         id: queenId,
+//         name: queenName
+//     }
 
-    queens.push(queenObject)
-}
+//     queens.push(queenObject)
+// }
 
-const hailTheQueen = (nameString) => {
-    return `Hail Her Majesty, ${nameString}.`
-}
+// const hailTheQueen = (nameString) => {
+//     return `Hail Her Majesty, ${nameString}.`
+// }
 
-const payTribute = (tributeId, tributeDescription, queenId) => {
-    const tribute = {
-        id: tributeId, 
-        description: tributeDescription, 
-        queenId: queenId
-    }
+// const payTribute = (tributeId, tributeDescription, queenId) => {
+//     const tribute = {
+//         id: tributeId, 
+//         description: tributeDescription, 
+//         queenId: queenId
+//     }
     
-    tributeChest.push(tribute)
-}
+//     tributeChest.push(tribute)
+// }
 
-createQueen(1, "Trinity Terry")
-createQueen(2, "Sydney Noh")
-createQueen(3, "Christina Ashworth")
-createQueen(4, "Aja Washington")
+// createQueen(1, "Trinity Terry")
+// createQueen(2, "Sydney Noh")
+// createQueen(3, "Christina Ashworth")
+// createQueen(4, "Aja Washington")
 
-for (const queen of queens) {
-    const hailMessage = hailTheQueen(queen.name)
-    console.log(hailMessage)
-}
+// for (const queen of queens) {
+//     const hailMessage = hailTheQueen(queen.name)
+//     console.log(hailMessage)
+// }
 
-    payTribute(1, "Barrel of wine", 2)
-    payTribute(2, "Silk", 1)
-    payTribute(3, "Bushel of oranges", 3)
-    payTribute(4, "Ten gold coins", 1)
+//     payTribute(1, "Barrel of wine", 2)
+//     payTribute(2, "Silk", 1)
+//     payTribute(3, "Bushel of oranges", 3)
+//     payTribute(4, "Ten gold coins", 1)
 
-    for (const queen of queens) {
+//     for (const queen of queens) {
 
-    for (const tribute of tributeChest) {
-        if (tribute.queenId === queen.id) {
-        console.log(`${queen.name} has tribute ${tribute.description}`)
-    }
-}
-    }
+//     for (const tribute of tributeChest) {
+//         if (tribute.queenId === queen.id) {
+//         console.log(`${queen.name} has tribute ${tribute.description}`)
+//     }
+// }
+//     }
